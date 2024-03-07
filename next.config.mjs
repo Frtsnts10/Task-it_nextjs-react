3d9754ab7f11002e748659fbb2aef32d077ecf7d
Filbert <filbert10.work@gmail.com>
2024-03-07 18:01:37 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    // remotePatterns: ["https://img.clerk.com"],
     domains: ["localhost", "https://img.clerk.com", "img.clerk.com"],
   },
 };
